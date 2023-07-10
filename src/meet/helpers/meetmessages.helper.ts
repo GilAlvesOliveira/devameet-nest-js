bf8d5ccb5c169dc13a5059aeb52d1e2e1f98be60
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-07-10 12:53:38 -0300

    Gerenciamento de salas: Atualização das salas com objetos Parte 3

--- src/meet/helpers/meetmessages.helper.ts
+++ src/meet/helpers/meetmessages.helper.ts
@@ -1,4 +1,9 @@
 export const MeetMessagesHelper = {
     CREATE_NAME_NOT_VALID: "Nome da reunião é inválido",
     CREATE_COLOR_NOT_VALID: "Cor da reunião é inválido, precisa ser um hexadecimal",
+    UPDATE_OBJECT_NAME_NOT_VALID: "Nome do objeto é inválido",
+    UPDATE_XY_NOT_VALID: "Eixo x e y do objeto deve ser entree 0 e 8",
+    UPDATE_ZINDEX_NOT_VALID: "Zindex inválido",
+    UPDATE_ORIENTATION_NOT_VALID: "Orientação inválida",
+    UPDATE_MEET_NOT_FOUND: "reunião não encontrada",
 }
--- src/meet/meet.controller.ts
+++ src/meet/meet.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Request } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Request } from '@nestjs/common';
 import { CreateMeetDto } from './dtos/createmeet.dto';
 import { GetMeetDto } from './dtos/getmeet.dto';
 import { MeetService } from './meet.service';
+import { UpdateMeetDto } from './dtos/updatemeet.dto';
 
 @Controller('meet')
 export class MeetController {
@@ -34,5 +35,19 @@ export class MeetController {
         const { userId } = req?.user;
         const { id } = params;
         await this.service.deleteMeetByUser(userId, id);
+    }
+
+    @Get('object/:id')
+    async getObjectsByMeetId(@Request() req, @Param() params){
+        const { userId } = req?.user;
+        const { id } = params;
+        return await this.service.getMeetObjects(id, userId);
+    }
+
+    @Put(':id')
+    async updateMeet(@Request() req, @Param() params, @Body() dto: UpdateMeetDto){
+        const { userId } = req?.user;
+        const { id } = params;
+        await this.service.update(id, userId, dto);
     }
 }
